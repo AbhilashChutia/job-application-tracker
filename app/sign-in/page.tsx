3f8467bedc80b2701fd592a28d7be5b607f3fcc0
Abhilash <abhilashchutia1999@gmail.com>
2026-06-14 03:27:47 +0530

    mongodb | mongoose | better-auth | login | logut | authentication

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,7 +1,119 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { signIn } from "@/lib/auth/auth-client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const router = useRouter();
+
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        setError("");
+        setLoading(true);
+
+        try {
+            const result = await signIn.email({
+                email,
+                password,
+            });
+
+            if (result.error) {
+                setError(result.error.message ?? "Failed to sign in");
+            } else {
+                router.push("/dashboard");
+            }
+        } catch (err) {
+            setError("An unexpected error has occurred.");
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
-        <div>
-            <h1>Sign In</h1>
+        <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4 ">
+            <Card className="w-full max-w-md border-gray-200 shadow-lg">
+                <CardHeader className="space-y-1">
+                    <CardTitle className="text=2xl font-bold text-black">
+                        Sign In
+                    </CardTitle>
+                    <CardDescription className="text-gray-600">
+                        Enter your credentials to access your account
+                    </CardDescription>
+                </CardHeader>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <CardContent className="space-y-4">
+                        {error && (
+                            <div className="rounded-md bg-destructive/15 p-3 text-sm text-destructive">
+                                {error}
+                            </div>
+                        )}
+                        <div>
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                placeholder="john@example.com"
+                                required
+                                className="border-gray-300 focus:border-primary focus:ring-primary"
+                            />
+                        </div>
+
+                        <div>
+                            <Label htmlFor="password">Password</Label>
+                            <Input
+                                id="password"
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                placeholder="John Doe"
+                                required
+                                minLength={8}
+                                className="border-gray-300 focus:border-primary focus:ring-primary"
+                            />
+                        </div>
+                    </CardContent>
+                    <CardFooter className="flex flex-col space-y-4">
+                        <Button
+                            type="submit"
+                            className="w-full bg-primary hover:bg-primary/90"
+                            disabled={loading}
+                        >
+                            {loading ? "Siging in..." : "Sign In"}
+                        </Button>
+                        <p>
+                            Don't have an account?{" "}
+                            <Link
+                                href="/sign-up"
+                                className="font-medium text-primary hover:underline"
+                            >
+                                Sign Up
+                            </Link>{" "}
+                        </p>
+                    </CardFooter>
+                </form>
+            </Card>
         </div>
     );
 }
